10b9bb4b6d6f3d9ff2d32406b5813755e8834750
nithin1305 <nithin1305@hotmail.com>
2023-04-18 17:06:12 +0530

    Updated

--- print.sh
+++ print.sh
@@ -1,1 +1,3 @@
-echo -e "\e[31mRed\e[32mGreen\e[33mYellow\e[34mBlue\e[35mMagents\e[36mCyan\e[0m"
+echo -e "\e[31mRed\e[32mGreen\e[33mYellow\e[34mBlue\e[35mMagenta\e[36mCyan\e[0m"
+
+echo -e "\e[1;31mRed\e[1;32mGreen\e[1;33mYellow\e[1;34mBlue\e[1;35mMagenta\e[1;36mCyan\e[0m"
