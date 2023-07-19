98d61dae805a8ef8ab6666cd8254e125789f93f8
nithin1305 <nithin1305@hotmail.com>
2023-07-19 17:37:23 +0530

    roboshop 8

--- Terraform-Practice/roboshop/main.tf
+++ Terraform-Practice/roboshop/main.tf
@@ -1,43 +1,43 @@
-# module "frontend" {
-#   source = "./frontend"
-# }
+module "frontend" {
+  source = "./frontend"
+}
 
 module "cart" {
   source = "./cart"
 }
 
-# module "catalogue" {
-#   source = "./catalogue"
-# }
+module "catalogue" {
+  source = "./catalogue"
+}
 
-# module "payment" {
-#   source = "./payment"
-# }
+module "payment" {
+  source = "./payment"
+}
 
-# module "shipping" {
-#   source = "./shipping"
-# }
+module "shipping" {
+  source = "./shipping"
+}
 
-# module "cart" {
-#   source = "./cart"
-# }
+module "cart" {
+  source = "./cart"
+}
 
-# module "mysql" {
-#   source = "./mysql"
-# }
+module "mysql" {
+  source = "./mysql"
+}
 
-# module "mongodb" {
-#   source = "./mongodb"
-# }
+module "mongodb" {
+  source = "./mongodb"
+}
 
-# module "redis" {
-#   source = "./redis"
-# }
+module "redis" {
+  source = "./redis"
+}
 
-# module "rabbitmq" {
-#   source = "./rabbitmq"
-# }
+module "rabbitmq" {
+  source = "./rabbitmq"
+}
 
-# module "dispatch;" {
-#   source = "./dispatch;"
-# }
+module "user" {
+  source = "./user"
+}
