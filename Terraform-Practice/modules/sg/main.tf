3fbe4969c3e81ea252888a403517b53348ef9a69
nithin1305 <nithin1305@hotmail.com>
2023-07-19 15:56:13 +0530

    Commit 1

---
+++ Terraform-Practice/modules/sg/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow-tls" {
+  name = "allow-tls"
+  description = "Allow tls inbound traffic"
+
+  ingress = {
+    description = "TLS from VPC"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "allow-tls"
+  }
+}
+
+output "SG_ID" {
+  value = aws_security_group.allow-tls.id
+}
