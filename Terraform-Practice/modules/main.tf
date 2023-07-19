15de8b058ae3ffa4e1705ed242efdc0d8b8425ca
nithin1305 <nithin1305@hotmail.com>
2023-07-19 16:01:26 +0530

    commit 2

--- Terraform-Practice/modules/main.tf
+++ Terraform-Practice/modules/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "nithinbucketterraform"
+    key    = "practice"
+    region = "us-east-1"
+  }
+}
+
 module "ec2" {
   source = "./ec2"
   SG_ID = module.sg.SG_ID
