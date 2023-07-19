95399fc3691136cb400f8d5c10ed5917ad888bdf
nithin1305 <nithin1305@hotmail.com>
2023-07-19 17:08:06 +0530

    roboshop 3

--- Terraform-Practice/roboshop/module/ec2.tf
+++ Terraform-Practice/roboshop/module/ec2.tf
@@ -1,5 +1,16 @@
-resource "aws_instance" "ec2" {
-  ami = "ami-0bb6af715826253bf"
+# resource "aws_instance" "ec2" {
+#   ami = "ami-0bb6af715826253bf"
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids = [aws_security_group.allow-app.id]
+
+#   tags = {
+#     Name = var.COMPONENT
+#   }
+# }
+
+# Request a spot instance at $0.03
+resource "aws_spot_instance_request" "ec2" {
+  ami           = "ami-0bb6af715826253bf"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow-app.id]
 
