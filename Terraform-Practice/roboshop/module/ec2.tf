ba7da5ccc4af2c376bce2e9d3b75f6d60d2e6c66
nithin1305 <nithin1305@hotmail.com>
2023-07-19 17:17:34 +0530

    roboshop 4

--- Terraform-Practice/roboshop/module/ec2.tf
+++ Terraform-Practice/roboshop/module/ec2.tf
@@ -9,12 +9,19 @@
 # }
 
 # Request a spot instance at $0.03
-resource "aws_spot_instance_request" "ec2" {
+resource "aws_spot_instance_request" "cheap_worker" {
   ami           = "ami-0bb6af715826253bf"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow-app.id]
+  wait_for_fulfillment = true
 
   tags = {
     Name = var.COMPONENT
   }
+}
+
+resource "aws_ec2_tag" "ec2_name_tag" {
+  resource_id = aws_spot_instance_request.cheap_worker.spot_instance_id
+  key         = "Name"
+  value       = var.COMPONENT
 }
